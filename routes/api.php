52a1d21bfc6414934ded746ef6f449c373124ee2
novak <novakmzv@gmail.com>
2025-08-29 01:10:28 -0500

    feat: implemented notifications route 🚚

---
+++ routes/api.php
@@ -0,0 +1,11 @@
+<?php
+
+use App\Http\Controllers\NotificationController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+Route::post('notifications', [NotificationController::class, 'store']);
