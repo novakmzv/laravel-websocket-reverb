3fe7a1cd2f8681bb8a84a28e184bbe88245639ab
novak <novakmzv@gmail.com>
2025-08-29 01:00:22 -0500

    feat: added NotificationSent 🔨

---
+++ app/Events/NotificationSent.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Events;
+
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+class NotificationSent implements ShouldBroadcast
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    public string $message;
+    public string $user;
+
+    /**
+     * Create a new event instance.
+     */
+    public function __construct(string $message, string $user = 'Sistema')
+    {
+        $this->message = $message;
+        $this->user = $user;
+    }
+
+    /**
+     * Get the channels the event should broadcast on.
+     *
+     * @return array<int, Channel>
+     */
+    public function broadcastOn(): array
+    {
+        return [
+            new Channel('notifications'),
+        ];
+    }
+}
